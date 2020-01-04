d5cedacb56574e169c001f6327e2c93cff7c4371
carlosescom <carlos.escom.ipn@gmail.com>
2020-01-03 22:44:13 -0600

    Updates

---
+++ precache-manifest.fbc7c069a9676d7870527f9695a89e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f22e914fe3beb33226a1",
+    "url": "/College-Collectible-Cards/static/js/runtime~main.a01f6723.js"
+  },
+  {
+    "revision": "29d34861390d65562a8d",
+    "url": "/College-Collectible-Cards/static/js/main.cf561c35.chunk.js"
+  },
+  {
+    "revision": "9ea4df72883244193910",
+    "url": "/College-Collectible-Cards/static/js/2.f86af1d0.chunk.js"
+  },
+  {
+    "revision": "29d34861390d65562a8d",
+    "url": "/College-Collectible-Cards/static/css/main.628fa68a.chunk.css"
+  },
+  {
+    "revision": "a55b3ee2549fe049e4f2cb462868408a",
+    "url": "/College-Collectible-Cards/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ccc/precache-manifest.9a78de755e71935e1f0cc8921b415236.js"
+  "/College-Collectible-Cards/precache-manifest.fbc7c069a9676d7870527f9695a89e80.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ccc/index.html", {
+workbox.routing.registerNavigationRoute("/College-Collectible-Cards/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
